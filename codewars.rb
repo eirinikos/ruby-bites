a9681536ccc64dedae365760d3ff4af08f9962c8
Andrea <eirinikos@gmail.com>
2015-10-30 00:19:57 -0700

    work in progress

--- codewars.rb
+++ codewars.rb
@@ -1,3 +1,33 @@
+# codewars kata: enigma machine (plugboard)
+# http://www.codewars.com/kata/5523b97ac8f5025c45000900/train/ruby
+
+class Plugboard
+  def initialize(wires)
+    if wires.size.even? && (wires.size/2) < 11 && wires.split(%r{\s*})==wires.split(%r{\s*}).uniq
+      # what about a string of odd-numbered size that includes a valid number of A-Z pairs, plus some non-A-Z characters?
+      @wires = wires
+    else
+      "Invalid input!"
+    end
+  end
+  def process(wire)
+    # find wire in @wires
+    # if wire is not within @wires, return wire unchanged
+    # if wire is not within A-Z, return wire unchanged
+  end
+end
+
+
+# codewars kata: pentabonacci
+# http://www.codewars.com/kata/55c9172ee4bb15af9000005d/train/ruby
+
+def count_odd_pentaFib(n)
+  array = [0,1,1,2,4,8]
+  (5..n).each do |i|
+    array[i] = array[i-5] + array[i-4] + array[i-3] + array[i-2] + array[i-1]
+  end
+  array.select{|i| i.odd?}.uniq!.count
+end
 
 
 # codewars kata: counting in the amazon
