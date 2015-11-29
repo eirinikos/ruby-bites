d148e6662a181c1cb5d4b4378fefa011a2bd5e5d
Andrea <eirinikos@gmail.com>
2015-11-29 00:23:46 -0800

    work in prog

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,25 +1,41 @@
-# initialize the board with nil values
-board = Array.new(3) {Array.new(3)}
+class TTT
+  attr_accessor :board
 
-# print board in a human-readable format
-# instruct player 1 to make first move
-def print_board(board)
-  if board.none? {|row| row.any? {|i| i }}
-    puts "\n   |   |    \n-----------\n   |   |    \n-----------\n   |   |    \n\n"
-    "Player 1, make your move!"
-  else
-    puts "\n"
-    board.each do |row| 
-      puts " #{row[0]} | #{row[1]} | #{row[2]} ",
-      "-----------"
+  # initialize the TTT board with nil values
+  def initialize
+    @board = Array.new(3) {Array.new(3)}
+  end
+
+  # print board in a human-readable format
+  # instruct player 1 to make first move
+  def print_board
+    if @board.none? {|row| row.any? {|i| i }}
+      puts "\n 1 | 2 | 3  \n-----------\n 4 | 5 | 6  \n-----------\n 7 | 8 | 9  \n\n"
+      play_first_move 
+    else
+      puts "\n"
+      @board.each do |row| 
+        puts " #{row[0]} | #{row[1]} | #{row[2]} ",
+        "-----------"
+      end
+      "#{}, the next move is yours! Select a move: \n"
     end
-    "Next move!"
+  end
+
+  def play_first_move
+    puts "Player 1, make your move!\nDo you want to play an X or an O?"
+    player_1 = gets.chomp
+    puts "Excellent choice. In which box would you like to place your #{player_1}?\n"
+    player_1_place = gets.chomp
+    # handle selection
+  end
+
+  def play_next_move
   end
 end
 
-
-# player 1 makes a move, get user input and update matrix
-# player 2 makes a move, get user input and update matrix
+# player 1 makes a move, get user input and update board
+# player 2 makes a move, get user input and update board
 # repeat until someone marks 3 Os or 3 Xs in a row (horizontal, vertical, or diagonal)
 
 # horizontal victory for a given row = row[0] == row[1] == row[2]
