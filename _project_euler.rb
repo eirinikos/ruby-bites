536b54cded1b464e49b499add515ba274d518033
Andrea <eirinikos@gmail.com>
2015-12-19 23:16:37 -0800

    solve Proj Euler problem #6

--- _project_euler.rb
+++ _project_euler.rb
@@ -1,3 +1,18 @@
+# project euler #6:
+# https://projecteuler.net/problem=6
+# the sum of the squares of the first ten natural numbers is:
+# 1**2 + 2**2 + ... + 10**2 = 385
+# the square of the sum of the first ten natural numbers is:
+# (1 + 2 + ... + 10)**2 = 552 = 3025
+# hence, the difference between the sum of the squares of
+# the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+# find the difference between the sum of the squares of the first one hundred
+# natural numbers and the square of the sum.
+
+(1..100).map{ |n| n**2 }.reduce(&:+)
+(1..100).reduce(&:+)**2
+
+
 # project euler #5:
 # https://projecteuler.net/problem=5
 # 2520 is the smallest number that can be divided by
