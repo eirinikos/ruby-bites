cca4c578ad6491121e1660fcb0f1da77b28573fb
Andrea <eirinikos@gmail.com>
2016-01-14 12:46:19 -0800

    fix bug that was introduced with .rjust

--- tictactoe.rb
+++ tictactoe.rb
@@ -3,26 +3,26 @@ class TTT
     @player_1, @player_2, @computer, @human, @turn, @dimension, @board = nil
   end
 
-  def print_three_board
-    puts "\n #{@board[0]} | #{@board[1]} | #{@board[2]} ",
-        "----------",
-        " #{@board[3]} | #{@board[4]} | #{@board[5]} ",
-        "----------",
-        " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-  end
-
-  def print_four_board
-    puts "\n  #{@board[0]} |  #{@board[1]} |  #{@board[2]} |  #{@board[3]}",
-        "-------------------",
-        "  #{@board[4]} |  #{@board[5]} |  #{@board[6]} |  #{@board[7]}",
-        "-------------------",
-        "  #{@board[8]} | #{@board[9]} | #{@board[10]} | #{@board[11]} ",
-        "-------------------",
-        " #{@board[12]} | #{@board[13]} | #{@board[14]} | #{@board[15]} "
+  def print_board
+    if @dimension == 3
+      puts "\n #{@board[0]} | #{@board[1]} | #{@board[2]} ",
+          "----------",
+          " #{@board[3]} | #{@board[4]} | #{@board[5]} ",
+          "----------",
+          " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    else
+      puts "\n  #{@board[0]} |  #{@board[1]} |  #{@board[2]} |  #{@board[3]}",
+          "-------------------",
+          "  #{@board[4]} |  #{@board[5]} |  #{@board[6]} |  #{@board[7]}",
+          "-------------------",
+          "  #{@board[8]} | #{@board[9]} | #{@board[10]} | #{@board[11]} ",
+          "-------------------",
+          " #{@board[12]} | #{@board[13]} | #{@board[14]} | #{@board[15]} "
+    end
   end
 
   def available?(box)
-    box.class == Fixnum
+    box.to_i != 0
   end
 
   def update_board(player, place)
@@ -43,12 +43,12 @@ class TTT
     print "Enter '3' for the former or '4' for the latter: "
     @dimension = gets.chomp.to_i
     if @dimension == 3
-      @board = (1..9).to_a
+      @board = %w{1 2 3 4 5 6 7 8 9}
     elsif @dimension == 4
-      @board = (1..16).to_a
+      @board = %w{1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16}
     else
       @dimension = 3
-      @board = (1..9).to_a
+      @board = %w{1 2 3 4 5 6 7 8 9}
       print "That's not a valid choice, so we'll just default to a 3 x 3 board.\n"
     end
 
@@ -81,7 +81,7 @@ class TTT
   end
 
   def relay_computer_human_turns
-    @dimension == 3 ? print_three_board : print_four_board
+    print_board
 
     if @computer == 0 && @human.nil? # human plays first move of the game
       print "\nHuman, do you want to play an X or an O? "
@@ -106,7 +106,7 @@ class TTT
   end
 
   def relay_player_turns
-    @dimension == 3 ? print_three_board : print_four_board
+    print_board
     
     if @player_1.nil?
       print "\nPlayer 1, make your move!\nDo you want to play an X or an O? "
@@ -155,14 +155,14 @@ class TTT
   end
 
   def prompt_new_box(player)
-    @dimension == 3 ? print_three_board : print_four_board
+    print_board
     print "\nSTOP in the name of the law! That box is occupied - please choose another one. "
     mark(player)
   end
 
   def determine_game_over(player)
     if victory?
-      @dimension == 3 ? print_three_board : print_four_board
+      print_board
       
       if @turn == @player_1 || @turn == @player_2 || @turn == @human
         puts "\nCONGRATULATIONS! You have vanquished your foe!\n\n(╯°□°)╯︵ ┻━┻\n\n"
@@ -185,7 +185,7 @@ class TTT
   end
 
   def victory?
-    sliced_board = @board.each_slice(@dimension).to_a
+    sliced_board = @board.map{ |box| box.strip }.each_slice(@dimension).to_a
     reversed_sliced_board = sliced_board.map{ |a| a.reverse }
 
     sliced_board.any?{ |row| row.uniq.size == 1 } ||
