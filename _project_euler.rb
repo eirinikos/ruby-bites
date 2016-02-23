b3b1420a8ee756ad050e6eb269a024b159145b40
Andrea <eirinikos@gmail.com>
2016-02-22 21:33:47 -0800

    solve Proj Euler 13

--- _project_euler.rb
+++ _project_euler.rb
@@ -138,34 +138,12 @@ end
 
 circular_primes.count
 
-# project euler #9:
-# # https://projecteuler.net/problem=9
+# project euler #13:
+# # https://projecteuler.net/problem=13
+# find the first 10 digits of the sum of the given one-hundred 50-digit numbers.
 
-# a Pythagorean triplet is a set of three natural numbers, a < b < c,
-# for which a**2 + b**2 == c**2.
-# e.g., 3**2 + 4**2 = 9 + 16 = 25 = 5**2.
-# there exists exactly one Pythagorean triplet for which a + b + c = 1000.
-# find the product abc.
+string.lines.map(&:chomp).map(&:to_i).reduce(&:+).to_s[0..9]
 
-
-a**2 + b**2 == c**2
-a + b + c == 1000
-
-a == 1000 - (b + c)
-b == 1000 - (a + c)
-c == 1000 - (a + b)
-
-6, 8, 10
-9, 12, 15
-12, 16, 20
-240, 320, 400
-246, 328, 410
-
-249, 332, 415
-
-# create an array of triplet arrays
-
-(1..1000).to_a.map{|n| }
 
 # project euler #8:
 # # https://projecteuler.net/problem=8
